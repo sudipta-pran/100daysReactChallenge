3a957b4a6eb7ff11a4463037eff9451eb11d06fa
Sudipta Bhattacharyya <jinturocks@gmail.com>
2019-03-11 15:22:54 +0530

    day019 2nd commit

--- day019/src/App.js
+++ day019/src/App.js
@@ -1,26 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import NoMatch from './components/NoMatch'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <nav>
+            <ul>
+              <li><Link to="/">Home</Link></li>  
+              <li><Link to="/about">About</Link></li> 
+              <li><Link to="/contact">Contact</Link></li> 
+            </ul>  
+          </nav>  
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route component={NoMatch} />
+          </Switch>
+        </div>        
+      </BrowserRouter>
     );
   }
 }
